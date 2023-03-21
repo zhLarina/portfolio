57dc82913ba9cbae935d6d7dc87f5b0b21f37806
Janna Larina <zh.larina97@gmail.com>
2023-03-21 21:38:44 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let wrapperButton = document.querySelector(".wrapper-burger-btn");
+let menuButton = document.querySelector(".menu-button");
+let wrapperBurger = document.querySelector(".wrapper-burger");
+
+wrapperButton.onclick = function () {
+  wrapperBurger.classList.remove("burger-show");
+  wrapperButton.classList.remove("burger-show");
+};
+
+menuButton.onclick = function () {
+  wrapperBurger.classList.add("burger-show");
+  menuButton.classList.add("burger-show");
+};
+
+// const menu = function () {
+//   wrapperBurger.classList.add("burger-show");
+//   menuButton.classList.add("burger-show");
+// };
+// menuButton.addEventListener("click", menu);
